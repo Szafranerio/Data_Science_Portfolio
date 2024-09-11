9d9cd584479695ec3dfdf45459d72e2dd5ae7d8d
Bartlomiej Szafran <bartekszafran@icloud.com>
2024-09-11 19:28:30 +0200

    update

--- Weather_Mailing/weather.py
+++ Weather_Mailing/weather.py
@@ -50,55 +50,51 @@ else:
 
 
 # Mailing
-#mail = 'YOUR_EMAIL'
-#password = 'YOUR_PASSWORD'
-#recipients = ['RECIPIENT MAILS']
-#subject = 'Weather forecast'
-#smtp_server = 'smtp.PLATFORM.COM'
-#smtp_port = 587
-#
-## Create the email message
-#msg = MIMEMultipart()
-#msg['From'] = mail
-#msg['To'] = ', '.join(recipients)
-#msg['Subject'] = subject
-#
-## Email body
-#if will_rain:
-#    body = f'Take an umbrella, it will rain today. The temperature is around {cel_temp}°C. The wind is {wind} meters per second. Humidity is {humidity_data}%, and the pressure is {pressure_data} hPa.'
-#else:
-#    body = f'Today is sunny, the temperature is around {cel_temp}°C. The wind is {wind} meters per second. Humidity is {humidity_data}%, and the pressure is {pressure_data} hPa.'
-#msg.attach(MIMEText(body, 'plain'))
-#
-## List of image filenames, transfer from other file called analysis.py
-#filenames = ['temperature.png', 'humidity.png', 'windspeed.png']
-#paths = [os.path.join(os.getcwd(), filename) for filename in filenames]
-#
-## Attach each image file
-#for attachment_path in paths:
-#    if os.path.isfile(attachment_path):
-#        try:
-#            print(f"Attaching {attachment_path}")
-#            with open(attachment_path, 'rb') as img_file:
-#                img_data = img_file.read()
-#                image = MIMEImage(
-#                    img_data, name=os.path.basename(attachment_path))
-#                msg.attach(image)
-#        except Exception as e:
-#            print(f"Error processing file {attachment_path}: {e}")
-#    else:
-#        print(f"File not found: {attachment_path}")
-## Send the email
-#try:
-#    with smtplib.SMTP(smtp_server, smtp_port) as connection:
-#        connection.starttls()
-#        connection.login(user=mail, password=password)
-#        connection.sendmail(
-#            from_addr=mail,
-#            to_addrs=recipients,
-#            msg=msg.as_string()
-#        )
-#    print('Email with images was sent successfully!')
-#except Exception as e:
-#    print(f'Error sending email: {e}')
-#
+mail = 'YOUR_EMAIL'
+password = 'YOUR_PASSWORD'
+recipients = ['RECIPIENT MAILS']
+subject = 'Weather forecast'
+smtp_server = 'smtp.PLATFORM.COM'
+smtp_port = 587
+
+# Create the email message
+msg = MIMEMultipart()
+msg['From'] = mail
+msg['To'] = ', '.join(recipients)
+msg['Subject'] = subject
+
+# Email body
+if will_rain:
+    body = f'Take an umbrella, it will rain today. The temperature is around {cel_temp}°C. The wind is {wind} meters per second. Humidity is {humidity_data}%, and the pressure is {pressure_data} hPa.'
+else:
+    body = f'Today is sunny, the temperature is around {cel_temp}°C. The wind is {wind} meters per second. Humidity is {humidity_data}%, and the pressure is {pressure_data} hPa.'
+msg.attach(MIMEText(body, 'plain'))
+
+# List of image filenames, transfer from other file called analysis.py
+filenames = ['temperature.png', 'humidity.png', 'windspeed.png']
+paths = [os.path.join(os.getcwd(), filename) for filename in filenames]
+
+# Attach each image file
+for attachment_path in paths:
+    if os.path.isfile(attachment_path):
+        try:
+            print(f"Attaching {attachment_path}")
+            with open(attachment_path, 'rb') as img_file:
+                img_data = img_file.read()
+                image = MIMEImage(
+                    img_data, name=os.path.basename(attachment_path))
+                msg.attach(image)
+        except Exception as e:
+            print(f"Error processing file {attachment_path}: {e}")
+    else:
+        print(f"File not found: {attachment_path}")
+# Send the email
+    with smtplib.SMTP(smtp_server, smtp_port) as connection:
+        connection.starttls()
+        connection.login(user=mail, password=password)
+        connection.sendmail(
+            from_addr=mail,
+            to_addrs=recipients,
+            msg=msg.as_string()
+        )
+    print('Email with images was sent successfully!')
