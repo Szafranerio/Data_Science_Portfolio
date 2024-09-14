74d174c319cd3591067d988d9e52a33051329f0e
Bartlomiej Szafran <bartekszafran@icloud.com>
2024-09-14 16:06:07 +0200

    function updates

--- Stock_Market/Stock_market_analysis.py
+++ Stock_Market/Stock_market_analysis.py
@@ -4,8 +4,27 @@ from tkinter import messagebox, ttk, END
 FONT_NAME = "Courier"
 analysis = ['Price', 'RSI', 'MACD', 'Bollinger Bands', 'M10&M20', 'ALL']
 
-# UI
+# Functions
 
+
+def send_data():
+    ticker = input_ticker.get().title()
+    days = int(input_time.get())
+    mail = input_mail.get()
+
+    if len(ticker) == 0 or days == 0 or len(mail) == 0:
+        messagebox.showwarning(title="Warning", message="Check your data")
+    elif len(ticker) > 8:
+        messagebox.showwarning(title="Warning", message='Ticker to long')
+    elif days > 720:
+        messagebox.showwarning(title="Warning", message='Day period to long')
+    elif "@" not in mail:
+        messagebox.showwarning(title="Warning", message='Check your mail')
+
+        return
+
+
+# UI
 window = Tk()
 window.config(padx=25, pady=25)
 window.title('Stock Market Analysis')
@@ -26,7 +45,7 @@ time = Label(text='Time period in days (max 720)', font=(FONT_NAME, 12))
 time.grid(column=0, row=2)
 
 input_time = Entry(width=21)
-input_time.grid(column = 1, row=2)
+input_time.grid(column=1, row=2)
 
 email = Label(text='Write your email', font=(FONT_NAME, 12))
 email.grid(column=0, row=3)
@@ -47,8 +66,9 @@ for value in analysis:
 def selected_item():
     for i in listbox.curselection():
         print(listbox.get(i))
-        
-button = Button(text='Send', command=selected_item)
-button.grid(column=2,row=4)
+
+
+button = Button(text='Send', command=send_data)
+button.grid(column=2, row=4)
 
 window.mainloop()
