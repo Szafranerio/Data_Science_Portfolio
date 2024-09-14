e84c0c040ef29b4d326acf8e3a608c4414699d75
Bartlomiej Szafran <bartekszafran@icloud.com>
2024-09-14 14:43:41 +0200

    added input

--- Stock_Market/Stock_market_analysis.py
+++ Stock_Market/Stock_market_analysis.py
@@ -19,14 +19,30 @@ canvas.grid(column=1, row=0)
 stock = Label(text='Ticker Name', font=(FONT_NAME, 12))
 stock.grid(column=0, row=1)
 
+input_ticker = Entry(width=21)
+input_ticker.grid(column=1, row=1)
+
 time = Label(text='Time period in days (max 720)', font=(FONT_NAME, 12))
 time.grid(column=0, row=2)
+
+input_time = Entry(width=21)
+input_time.grid(column = 1, row=2)
 
 email = Label(text='Write your email', font=(FONT_NAME, 12))
 email.grid(column=0, row=3)
 
+input_mail = Entry(width=21)
+input_mail.grid(column=1, row=3)
+
 analysis_type = Label(text='Choose analysis type', font=(FONT_NAME, 12))
-time.grid(column=0, row=4)
+analysis_type.grid(column=0, row=4)
+
+
+
+
+
+
+
 
 
 window.mainloop()
