8e11d91b61e5a2ba8d0f60b9a6d880a04b582070
Bartlomiej Szafran <bartekszafran@icloud.com>
2024-09-14 14:33:21 +0200

    start project ui

--- Stock_Market/Stock_market_analysis.py
+++ Stock_Market/Stock_market_analysis.py
@@ -13,20 +13,4 @@ window.title('Stock Market Analysis')
 
 canvas = Canvas(width=500, height=500, highlightthickness=0)
 stock_png = PhotoImage(file='./images/stock_graph.png')
-canvas.create_image(250, 250, image=stock_png)
-canvas.grid(column=1, row=0)
-
-stock = Label(text='Ticker Name', font=(FONT_NAME, 12))
-stock.grid(column=0, row=1)
-
-time = Label(text='Time period in days (max 720)', font=(FONT_NAME, 12))
-time.grid(column=0, row=2)
-
-email = Label(text='Write your email', font=(FONT_NAME, 12))
-email.grid(column=0, row=3)
-
-analysis_type = Label(text='Choose analysis type', font=(FONT_NAME, 12))
-time.grid(column=0, row=4)
-
-
-window.mainloop()
+c
