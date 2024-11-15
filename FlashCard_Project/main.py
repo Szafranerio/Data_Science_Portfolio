7da669b97438b6a3070a0f744bf4571d06df7c2d
Bartlomiej Szafran <bartekszafran@icloud.com>
2024-11-15 20:12:07 +0100

    flashcards update

--- FlashCard_Project/main.py
+++ FlashCard_Project/main.py
@@ -25,6 +25,7 @@ BACKGROUND_COLOR = "#B1DDC6"
 current_card = {}
 to_learn = {}
 FONT_NAME = "Courier"
+locale.setlocale(locale.LC_ALL, 'da_DK.UTF-8')
 
 try:
     danish_data = pd.read_csv('./data/data/words_to_learn.csv')
@@ -53,17 +54,26 @@ def flip_card():
     canvas.itemconfig(card_title, text='English', fill='white')
     canvas.itemconfig(card_word, text=current_card['English'], fill='white')
     canvas.itemconfig(card_background, image=back_card)
+    
+def update_progress(value):
+    progress['value'] = value
+    window.update_idletasks()
+
+def increment_progress(current, total):
+    value = (current / total) * 100
+    update_progress(value)
+    
+
+total_words = len(to_learn) 
 
 def is_known():
-    global current_word_index, current_card
+    global current_card
     if current_card in to_learn:
         to_learn.remove(current_card)
-        current_word_index += 1
-        increment_progress(current_word_index, total_words)
         data = pd.DataFrame(to_learn)
         data.to_csv('./data/data/words_to_learn.csv', index=False)
         next_card()
-locale.setlocale(locale.LC_ALL, 'da_DK.UTF-8')
+        
 
 def show_data():
     show.show_data(window)
@@ -87,43 +97,45 @@ window.title('Flash Cards')
 
 flip_timer = window.after(5000, func=flip_card)
 
-canvas = Canvas(width=800, height=526, highlightthickness=0)
+canvas = Canvas(width=800, height=526, highlightthickness=0, bg=BACKGROUND_COLOR)
 front_card = PhotoImage(file='./data/images/card_front.png')
 back_card = PhotoImage(file='./data/images/card_back.png')
-card_background = canvas.create_image(400, 263, image=front_card)
+card_background = canvas.create_image(400, 263, image=front_card)  # Centered at the canvas
 card_title = canvas.create_text(
-    400, 150, text='Title', font=('Ariel', 40, 'italic'))
+    400, 150, text='Title', font=('Ariel', 40, 'italic'), fill='black')  # Centered at the top portion
 card_word = canvas.create_text(
-    400, 263, text='WORD', font=('Ariel', 60, 'bold'))
-canvas.config(bg=BACKGROUND_COLOR)
+    400, 263, text='WORD', font=('Ariel', 60, 'bold'), fill='black')  # Centered in the middle
 canvas.grid(column=0, row=0, columnspan=3)
-
-progress = ttk.Progressbar(window, orient=HORIZONTAL,
-                           length=300, mode='determinate')
-progress.grid(column=0, row=4, columnspan=3, pady=20)
 
 # Button images
 ok_button = PhotoImage(file='./data/images/right.png')
 false_button = PhotoImage(file='./data/images/wrong.png')
+speaker_button = PhotoImage(file='./data/images/speaker.png')
+
 correct_button = Button(image=ok_button, highlightbackground=BACKGROUND_COLOR,
                         highlightcolor=BACKGROUND_COLOR, highlightthickness=4, relief='solid', command=is_known)
 correct_button.grid(column=2, row=1)
+
 wrong_button = Button(image=false_button, highlightbackground=BACKGROUND_COLOR,
                       highlightcolor=BACKGROUND_COLOR, highlightthickness=4, relief='solid', command=next_card)
 wrong_button.grid(column=0, row=1)
 send_button = Button(text='Send to mail', highlightbackground=BACKGROUND_COLOR,
                      highlightcolor=BACKGROUND_COLOR, highlightthickness=4, relief='solid', command=send_to_mail)
-send_button.grid(column=0, row=2)
+send_button.grid(column=3, row=3)
 
 button_show = Button(text='Show data', highlightbackground=BACKGROUND_COLOR,
                      highlightcolor=BACKGROUND_COLOR, highlightthickness=4, relief='solid', command=show_data)
-button_show.grid(column=1, row=2)
+button_show.grid(column=3, row=0)
 
 edit_button_main = Button(text='Add/Edit/Delete Words', highlightbackground=BACKGROUND_COLOR,
                           highlightcolor=BACKGROUND_COLOR, highlightthickness=4, relief='solid', command=add_edit_or_delete_word)
-edit_button_main.grid(column=1, row=3)
+edit_button_main.grid(column=3, row=1)
 
-speak_button = Button(text='Speaker', highlightbackground=BACKGROUND_COLOR,
+random_button = Button(text='Random words game', highlightbackground=BACKGROUND_COLOR,
+                          highlightcolor=BACKGROUND_COLOR, highlightthickness=4, relief='solid')
+random_button.grid(column=3, row=2)
+
+speak_button = Button(image=speaker_button, highlightbackground=BACKGROUND_COLOR,
                       highlightcolor=BACKGROUND_COLOR, highlightthickness=4, relief='solid', command=say_word)
 speak_button.grid(column=1, row=1)
 
